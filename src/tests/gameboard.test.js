f4b47651596492e099b5c01649d9b4abfe2bee47
connorAlex <alex.connor.cochrane@gmail.com>
2022-08-31 13:56:52 -0500

    added receiveAttack Tests

--- src/tests/gameboard.test.js
+++ src/tests/gameboard.test.js
@@ -15,23 +15,45 @@ test("create a 10x10 grid", () => {
     expect(board.createGrid().length * board.createGrid()[0].length).toBe(100);
 });
 
+describe("ship placement", () => {
+    beforeAll(() => {
+        testShip = new ship(4);
+    });
 
-//gameboard places ships at specific coordinates on the board
-test("gameboard places ship at specific coordinates", () => {
-    board.placeShip(testShip,1,1, true);
-    expect(board.getGrid()[1][1]).toBe(testShip);
+    //gameboard places ships at specific coordinates on the board
+    test("gameboard places ship at specific coordinates", () => {
+        board.placeShip(testShip,1,1, true);
+        expect(board.getGrid()[1][1].ship).toBe(testShip);
+    });
+
+    test("entire ship is added to board", () => {
+        board.placeShip(testShip,0,0, true);
+        expect(board.getGrid()[0][3].ship).toBe(testShip);
+    });
+
+    test("ship can be place vertically", () => {
+        board.placeShip(testShip,0,0, false);
+        expect(board.getGrid()[3][0].ship).toBe(testShip);
+    });
+    
 });
 
-test("entire ship is added to board", () => {
-    board.placeShip(testShip,0,0, true);
-    expect(board.getGrid()[0][3]).toBe(testShip);
-});
+describe('detecting hits on board',() => {
+    let dummyShip;
+    beforeAll(() => {
+        
+        dummyShip = new ship(3);
+        board.placeShip(dummyShip,3,3,true);
+    });
 
-test("ship can be place vertically", () => {
-    board.placeShip(testShip,0,0, false);
-    expect(board.getGrid()[3][0]).toBe(testShip);
-})
-//receiveAttack function takes a pair of coordinates and determines if a hip is on that location
+    test("ship can be hit on grid", () => {
+        
+        board.receiveAttack(3,3);
+        console.log(board.getGrid()[3][3]);
+        expect(dummyShip.getPosition(0)).toBe(1);
+    });
+});
+//receiveAttack function takes a pair of coordinates and determines if a ship is on that location
 
 //receiveAttack sends the 'hit' function to the ship it hit
 
