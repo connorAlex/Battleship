9a4ea312ad56a4ca281dd6ad5721d49e819714a8
connorAlex <alex.connor.cochrane@gmail.com>
2022-08-31 12:21:06 -0500

    add placeHorizontalShip function

--- src/js/gameboard.js
+++ src/js/gameboard.js
@@ -1,14 +1,24 @@
+
 const gameboard = () => {
-    
+    let grid;
+
     const createGrid = () => {
-        let grid = new Array(10);
+        grid = new Array(10);
         for (let i = 0; i < 10; i++) {
             grid[i] = new Array(10);
         }
         return grid;
-    }
+    };
 
-    return {createGrid};
+    const placeHorizontalShip = (myShip, x, y) => {
+        for (let i = 0; i < myShip.getLength(); i++){
+            grid[x][y + i] = myShip;
+        };
+    };
+
+    const getGrid = () => grid;
+
+    return {createGrid,placeHorizontalShip, getGrid};
 
 };
 
