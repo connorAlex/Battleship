51a8ac9dd866cc4ae0d9f9969d0484aaf4d66a6a
connorAlex <alex.connor.cochrane@gmail.com>
2022-08-31 12:21:26 -0500

    added first test for placing horizontal ship

--- src/tests/gameboard.test.js
+++ src/tests/gameboard.test.js
@@ -1,24 +1,27 @@
 import {gameboard} from "../js/gameboard.js"
+import { ship } from "../js/ship.js";
 
 
 let board;
+let testShip;
 
 beforeAll(() => {
     board = new gameboard();
+    testShip = new ship(4);
 });
 
 //gameboard creates a grid to play on (10x10)
 test("create a 10x10 grid", () => {
-    
     expect(board.createGrid().length * board.createGrid()[0].length).toBe(100);
 });
-//gameboard creates a new ship
 
-//gameboard has list of all ships in play
 
 //gameboard places ships at specific coordinates on the board
-
-//recieveAttack function takes a pair of coordinates and determines if a hip is on that location
+test("gameboard places ship at specific coordinates", () => {
+    board.placeHorizontalShip(testShip,1,1);
+    expect(board.getGrid()[1][1]).toBe(testShip);
+});
+//receiveAttack function takes a pair of coordinates and determines if a hip is on that location
 
 //receiveAttack sends the 'hit' function to the ship it hit
 
