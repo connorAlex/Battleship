0923c06605d54877989c9debd6d8bb80c7fb6cef
connorAlex <alex.connor.cochrane@gmail.com>
2022-09-05 13:14:58 -0700

    add robotAttack to make a candom play

--- src/js/player.js
+++ src/js/player.js
@@ -4,8 +4,16 @@ const Player = () => {
 
     const attack = (x, y, board) => board.receiveAttack(x,y);
 
+    const robotAttack = (board) => {
+        let x = Math.round(Math.random() * 10);
+        let y = Math.round(Math.random() * 10);
+
+        board.receiveAttack(x,y);
+    };
+
     return {
         attack,
+        robotAttack
     }
 };
 
