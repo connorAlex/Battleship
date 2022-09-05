c0713c413e03b8f4f644c56dd1504cc9739b1d96
connorAlex <alex.connor.cochrane@gmail.com>
2022-09-05 08:40:20 -0500

    adjusted tests for proper (x,y) coordinate calls w/ grid

--- src/tests/gameboard.test.js
+++ src/tests/gameboard.test.js
@@ -4,16 +4,14 @@ import { ship } from "../js/ship.js";
 
 let board;
 let testShip;
-
-beforeAll(() => {
-    board = new gameboard();
-    testShip = new ship(4);
-});
+board = new gameboard();
 
 //gameboard creates a grid to play on (10x10)
 test("create a 10x10 grid", () => {
-    expect(board.createGrid().length * board.createGrid()[0].length).toBe(100);
+    expect(board.getGrid().length * board.getGrid()[0].length).toBe(100);
 });
+
+
 
 describe("ship placement", () => {
     beforeAll(() => {
@@ -22,13 +20,13 @@ describe("ship placement", () => {
 
     //gameboard places ships at specific coordinates on the board
     test("gameboard places ship at specific coordinates", () => {
-        board.placeShip(testShip,1,1, true);
-        expect(board.getGrid()[1][1].ship).toBe(testShip);
+        board.placeShip(testShip,0,0, true);
+        expect(board.getGrid()[0][0].shipIndex).toBe(0);
     });
 
     test("entire ship is added to board", () => {
         board.placeShip(testShip,0,0, true);
-        expect(board.getGrid()[0][3].ship).toBe(testShip);
+        expect(board.getGrid()[0][3].shipIndex).toBe(3);
     });
 
     test("ship can be place vertically", () => {
@@ -41,33 +39,36 @@ describe("ship placement", () => {
 describe('detecting hits on board',() => {
     let dummyShip;
     beforeAll(() => {
-        
-        dummyShip = new ship(3);
-        board.placeShip(dummyShip,3,3,true);
+        dummyShip = new ship(5);
+        board.placeShip(dummyShip,5,5,true);
     });
 
     test("ship can be hit on grid", () => {
-        
-        board.receiveAttack(3,3);
+        board.receiveAttack(5,5);
         expect(dummyShip.getPosition(0)).toBe(1);
     });
-    test("vertical ship can be hit on grid", () => {
-        let a = new ship(2);
-        board.placeShip(a, 0,0, false);
+
+    test.only("vertical ship can be hit on grid", () => {
+        let a = new ship(3);
+        board.placeShip(a, 0, 0, false);
         board.receiveAttack(0,2);
-        expect(a.getPosition(1)).toBe(1);
+        expect(a.getPosition(2)).toBe(1);
     });
-    test("a miss returns coordinates", () => {
-        expect(board.receiveAttack(9,9)).toEqual([9,9]);
-    })
+    
 });
-//receiveAttack function takes a pair of coordinates and determines if a ship is on that location
-
-//receiveAttack sends the 'hit' function to the ship it hit
-
-//receiveAttack returns coordinates of a missed shot
 
 //gameboard has record of missed shots coordinates
+describe('record missed shots', () => {
+    
+    beforeAll(()=> {
+        board.receiveAttack(1,1);
+        board.receiveAttack(1,2);
+    });
+
+    test("return list of all missed shots", () => {
+        expect(board.getMissedShots()).toEqual([[1,1],[1,2]]);
+    });
+});
 
 //gameboard hasWon function that determines if all ships have been sunk
 
