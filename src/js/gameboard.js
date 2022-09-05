b436acdeac3463a97908956a37cd787ff092e55d
connorAlex <alex.connor.cochrane@gmail.com>
2022-09-05 08:39:22 -0500

    debugged grid prototype issue

--- src/js/gameboard.js
+++ src/js/gameboard.js
@@ -2,42 +2,46 @@
 const gameboard = () => {
     
     const createGrid = () => {
-        let grid = new Array(10);
+        //let grid = Array.from(Array(10), () => new Array(10).fill({ship: undefined, shipIndex: undefined}));
+        let grid = [];
         for (let i = 0; i < 10; i++) {
-            grid[i] = new Array(10).fill({ship: undefined, shipIndex: undefined});
+            grid.push([]);
+            for (let j = 0; j < 10; j++){
+                grid[i][j] = {ship: undefined, shipIndex: undefined};
+            }
         }
         return grid;
     };
-
     let grid = createGrid();
+    let missedShots = [];
     
     const getGrid = () => grid;
 
     const placeShip = (myShip, x, y, isHorizontal) => {
         for (let i = 0; i < myShip.getLength(); i++){
-            if (isHorizontal) {
-                grid[x + i][y].ship = myShip;
-                grid[x + i][y].shipIndex = i;
+            if (isHorizontal === true) {
+                grid[y][(x + i)].ship = myShip;
+                grid[y][(x + i)].shipIndex = i;
             } else {
-                grid[x][y + i].ship = myShip
-                grid[x][y + i].shipIndex = i;
+                grid[(y + i)][x].ship = myShip
+                grid[(y + i)][x].shipIndex = i;
             }
             
         };
     };
 
     const receiveAttack = (x,y) => {
-        let cell = grid[x][y];
-        if((cell.ship !== undefined)) {
+        let cell = grid[y][x];
+        if(cell.ship != undefined) {
             cell.ship.hit(cell.shipIndex);
         } else{
-            return [x,y];
+            missedShots.push([x,y]);
         };
     };
 
-    
+    const getMissedShots = () => missedShots;
 
-    return {createGrid,placeShip, getGrid, receiveAttack};
+    return {createGrid,placeShip, getGrid, receiveAttack, getMissedShots};
 
 };
 
