fecba37eae4912f30d3cea194e6810def1071841
connorAlex <alex.connor.cochrane@gmail.com>
2022-09-13 17:27:42 -0700

    ducktape fix for negative number bug

--- src/js/player.js
+++ src/js/player.js
@@ -2,18 +2,40 @@ import {gameboard} from "../js/gameboard.js"
 
 const Player = () => {
 
-    const attack = (x, y, board) => board.receiveAttack(x,y);
+    let history = [];
 
-    const robotAttack = (board) => {
-        let x = Math.round(Math.random() * 10);
-        let y = Math.round(Math.random() * 10);
-
+    const attack = (x, y, board) => {
+        
         board.receiveAttack(x,y);
     };
+    
+    const getHistory = () => history;
+
+    const robotAttack = (board) => {
+        let coordinates = getRandomCoordinates();
+        console.log(coordinates);
+        history.push(coordinates);
+        board.receiveAttack(coordinates[0],coordinates[1]);
+    };
+
+    const getRandomCoordinates = () => {
+        let x = Math.abs(Math.round(Math.random() * 10 - 1));
+        let y = Math.abs(Math.round(Math.random() * 10 - 1));
+
+        let num = [x,y];
+
+        if (!history.includes(num)) {
+            return num;
+        }
+        else{
+            return getRandomCoordinates();
+        };
+    }
 
     return {
         attack,
-        robotAttack
+        robotAttack,
+        getHistory
     }
 };
 
