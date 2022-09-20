8e4c6abe8bdd26928c34ae5d7465ba81e93c60dc
connorAlex <alex.connor.cochrane@gmail.com>
2022-09-20 13:24:59 -0500

    add gameloop IIFE

--- src/js/game.js
+++ src/js/game.js
@@ -1,12 +1,23 @@
-const game = () => {
+import { gameboard } from "./gameboard.js";
+import { Player } from "./player.js";
+import { displayController } from "./display.js";
+import {ship} from "./ship.js";
+
+const game = (() => {
+    let player = Player();
+    let computer = Player();
+    let testShip = ship(3);
+    let body = document.querySelector("body");
+
+    displayController.setPlayer(player);
+    player.setEnemy(computer);
+    computer.setEnemy(player);
+    computer.getGameboard().placeShip(testShip,0,0,true);
+
+    // player.attack(0,0);
+    // player.attack(1,0);
     
-    const createGame = () => {
-
-    };
-
-    return {
-        createGame,
-    }
-};
-
-export {game};
+    
+    body.appendChild(displayController.updateBoard(player.getEnemyBoard().getGrid()));
+    body.appendChild(displayController.updateBoard(player.getGameboard().getGrid()));
+})();
