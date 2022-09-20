45048d2710770e38a1cd13c8cc7f3c8e62d8635b
connorAlex <alex.connor.cochrane@gmail.com>
2022-09-20 16:45:08 -0500

    added titles and missed markers

--- src/js/display.js
+++ src/js/display.js
@@ -1,18 +1,28 @@
 import { ship } from "../js/ship.js";
 import { gameboard } from "./gameboard.js";
+import { Player } from "./player.js";
 
 const displayController = (() => {
     let currentPlayer;
     let body = document.querySelector("body");
+    let grid;
 
     const setPlayer = (player) => {
       currentPlayer = player;
     };
 
-    const updateBoard = (grid) => {
+    const updateBoard = (player) => {
+      grid = player.getGameboard().getGrid();
       let boardParent = document.createElement("div");
+      let titleDiv = document.createElement("div");
+      let title =`${player.getName()}'s board`;
+
+      titleDiv.append(title);
+      boardParent.append(titleDiv);
       boardParent.classList.add("board");
 
+
+      //this should be refactored
       for (let i = 0; i < grid.length; i++) {
         let row = document.createElement("div");
         row.classList.add("row");
@@ -23,15 +33,18 @@ const displayController = (() => {
             if (e.target.parentNode.parentNode.nextElementSibling != null) selectCell(cell)
           });
           cell.classList.add("cell")
-
+          
           if (grid[i][j].hit === true) {
             markHit(cell);
+          } else if (grid[i][j].hit === false){
+            markMiss(cell);
           };
           row.appendChild(cell);
         };
-  
+        
         boardParent.appendChild(row);
       }
+
     
     return boardParent;
   };
@@ -56,13 +69,13 @@ const displayController = (() => {
     const x = cell.getAttribute("x");
     const y = cell.getAttribute("y");
     let boards = document.querySelectorAll(".board");
-
+    console.log(grid[y][x]);
     //this current player is why only the top board is changing
     currentPlayer.attack(x,y);
     currentPlayer.getEnemy().robotAttack();
     
-    body.replaceChild(updateBoard(currentPlayer.getEnemyBoard().getGrid()), boards[0]);
-    body.replaceChild(updateBoard(currentPlayer.getGameboard().getGrid()), boards[1]);
+    body.replaceChild(updateBoard(currentPlayer.getEnemy()), boards[0]);
+    body.replaceChild(updateBoard(currentPlayer), boards[1]);
   };
 
   const test = () => {
