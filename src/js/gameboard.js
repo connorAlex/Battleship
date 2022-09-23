4b9a16039fc1c52a68fddcfd16f50927c7ee1ac2
connorAlex <alex.connor.cochrane@gmail.com>
2022-09-23 15:07:18 -0500

    fixed auto placement verification bug

--- src/js/gameboard.js
+++ src/js/gameboard.js
@@ -2,59 +2,74 @@ const gameboard = () => {
   let grid = [];
   let missedShots = [];
   let shipCatalogue = [];
+
   const createGrid = () => {
     for (let i = 0; i < 10; i++) {
       grid.push([]);
       for (let j = 0; j < 10; j++) {
         grid[i][j] = { ship: undefined, shipIndex: undefined, hit: null};
-      }
-    }
+      };
+    };
     return grid;
   };
-  grid = createGrid();
-  
 
+  grid = createGrid();
   const getGrid = () => grid;
   const getMissedShots = () => missedShots;
   const getShips = () => shipCatalogue;
+
+  const validateCell = (x,y) => {
+    if (grid[y][x] === undefined){
+      return false;
+    };
+    return true;
+  }
 
   const placeShip = (myShip, x, y, isHorizontal) => {
     if (grid[y][x].ship != undefined){
       return false;
     };
+    for (let i = 0; i < myShip.getLength(); i++){
+      if (isHorizontal){
+        if (grid[y][x + i] === undefined) return false;
+      } else{
+        if(grid[y + i] === undefined) return false;
+      }
+    };
+    
     shipCatalogue.push(myShip);
+
     for (let i = 0; i < myShip.getLength(); i++) {
       if (isHorizontal === true) {
-        console.log(myShip);
         grid[y][x + i].ship = myShip;
         grid[y][x + i].shipIndex = i;
       } else {
         grid[y + i][x].ship = myShip;
         grid[y + i][x].shipIndex = i;
-      }
-    }
+      };
+    };
   };
-
+  
   const receiveAttack = (x, y) => {
     let cell = grid[y][x];
+
     if(cell.ship === undefined) {
       missedShots.push([x, y]);
       cell.hit = false;
     } else {
-      console.log("hit");
       cell.ship.hit(cell.shipIndex);
       cell.hit = true;
-      
-    }
+    };
   };
 
   
   const isAllDestroyed = () => {
+  
     for (let i = 0; i < shipCatalogue.length; i++) {
       if (shipCatalogue[i].isSunk() === false) {
         return false;
-      }
-    }
+      };
+    };
     return true;
   };
 
@@ -66,6 +81,7 @@ const gameboard = () => {
     getMissedShots,
     isAllDestroyed,
     getShips,
+    validateCell
   };
 };
 
