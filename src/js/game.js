7a5be03553535aadce6be5d359ef93d223cabbe1
connorAlex <alex.connor.cochrane@gmail.com>
2022-09-26 09:13:57 -0500

    prettier cleanup

--- src/js/game.js
+++ src/js/game.js
@@ -1,45 +1,41 @@
 import { gameboard } from "./gameboard.js";
 import { Player } from "./player.js";
 import { displayController } from "./display.js";
-import {ship} from "./ship.js";
+import { ship } from "./ship.js";
 
 const game = (() => {
-    let player = Player("player");
-    let computer = Player("computer");
-    let carrier = ship(5);
-    let battleship = ship(4);
-    let destroyer = ship(3);
-    let sub = ship(3);
-    let cruiser = ship(2);
-    let testShip = ship(3);
-    let ships = [
-      carrier,
-      battleship,
-      destroyer,
-      sub,
-      cruiser
-    ];
-    let direction = false;
-    let body = document.querySelector("body");
+  let player = Player("player");
+  let computer = Player("computer");
+  let carrier = ship(5);
+  let battleship = ship(4);
+  let destroyer = ship(3);
+  let sub = ship(3);
+  let cruiser = ship(2);
+  let ships = [carrier, battleship, destroyer, sub, cruiser];
+  let direction = false;
+  let body = document.querySelector("body");
 
-    player.setEnemy(computer);
-    computer.setEnemy(player);
-    
-    
-    while (computer.getGameboard().getShips().length < 5) {
-        let x = Math.floor(Math.random() * 10);
-        let y = Math.floor(Math.random() * 10);
-        
+  player.setEnemy(computer);
+  computer.setEnemy(player);
 
-        let ship = ships.at(-1);
-        if (computer.getGameboard().placeShip(ship,parseInt(x),parseInt(y),direction) === false){
-            continue;
-        }
-        ships.pop();
-        direction = !direction;
-    };
-    console.log(computer.getGameboard().getShips());
-    displayController.setPlayer(player);
-    body.appendChild(displayController.updateBoard(computer));
-    body.appendChild(displayController.updateBoard(player));
+  while (computer.getGameboard().getShips().length < 5) {
+    let x = Math.floor(Math.random() * 10);
+    let y = Math.floor(Math.random() * 10);
+    let ship = ships.at(-1);
+
+    if (
+      computer
+        .getGameboard()
+        .placeShip(ship, parseInt(x), parseInt(y), direction) === false
+    ) {
+      continue;
+    }
+
+    ships.pop();
+    direction = !direction;
+  }
+  console.log(computer.getGameboard().getShips());
+  displayController.setPlayer(player);
+  body.appendChild(displayController.updateBoard(computer));
+  body.appendChild(displayController.updateBoard(player));
 })();
